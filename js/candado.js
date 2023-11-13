42d33a7cd507c33a3f81461e9bd2a27a5631541d
Frankcb04 <frankecb07@gmail.com>
2023-11-13 02:50:54 -0500

    VerPedido, DetallePedido, GestionarDelivery

---
+++ js/candado.js
@@ -0,0 +1,19 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Agrega un evento de clic a todos los elementos con la clase "candado"
+    var candados = document.querySelectorAll(".candado");
+
+    candados.forEach(function (candado) {
+        candado.addEventListener("click", function () {
+            // Verifica la clase actual del icono
+            if (candado.innerHTML === "lock") {
+                // Cambia a la clase del icono abierto si está cerrado
+                candado.innerHTML = "lock_open";
+            } else {
+                // Cambia a la clase del icono cerrado si está abierto
+                candado.innerHTML = "lock";
+            }
+        });
+    });
+});
