39e73bf8578152d56cd76b27ba00112cf4d911c1
Flavio Villacorta <143514047+Flavicox18@users.noreply.github.com>
2023-11-14 15:55:45 -0500

    Agregando Validar cargo en inicio sesion empleado

--- js/validarSesion.js
+++ js/validarSesion.js
@@ -1,8 +1,10 @@
+// validarSesion.js
+
 function validarSesion() {
-    
     // Obtener referencias a los campos del formulario
     var correo = document.getElementById('correo');
     var contraseña = document.getElementById('contraseña');
+    var cargo = document.getElementById('categoria');
 
     // Verificar si los campos están vacíos
     if (correo.value === '') {
@@ -17,10 +19,25 @@ function validarSesion() {
         contraseña.classList.remove('error');
     }
 
-    // Realizar el registro si todos los campos están completos
+    // Realizar el inicio de sesión si todos los campos están completos
     if (correo.value !== '' && contraseña.value !== '') {
-        // Aquí puedes enviar el formulario o realizar otras acciones
-        alert('Se inició con éxito la sesión');
+        // Obtener el valor seleccionado en el campo "Cargo"
+        var cargoSeleccionado = cargo.value;
+
+        // Redirigir según el cargo seleccionado
+        switch (cargoSeleccionado) {
+            case "Administrador":
+                window.location.href = "MainAdministrador.html";
+                break;
+            case "Despachador":
+                window.location.href = "VerPedidos.html"; // Cambiar al nombre correcto de la interfaz
+                break;
+            case "Repartidor":
+                window.location.href = "VerPedidos.html"; // Cambiar al nombre correcto de la interfaz
+                break;
+            default:
+                alert("Cargo no reconocido");
+        }
     } else {
         // Detener la acción predeterminada del botón
         event.preventDefault();
